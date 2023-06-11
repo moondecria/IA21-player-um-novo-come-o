ba895290cd272c52a15f16c65e57eb2409d9cfcb
lua <104568826+moondecria@users.noreply.github.com>
2023-06-11 23:57:42 +0000

    feijão

--- jurema/http-server.js
+++ jurema/http-server.js
@@ -4,25 +4,28 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.use("/create", (req, res) => {
-    const { file, text } = req.query
-    fs.writeFileSync(file, text)
-    res.send(new Date()) // creado o arquivo tal tal tal
+app.use("/create", (req, res) => { 
+  const { file, texto } = req.query
+  fs.writeFileSync(file, texto)
+  res.send("Criado!!")
 })
 
-app.use("/read", (req, res) => {
-    const { file } = req.query
-    // fs.readFileSync
+app.use("/read", (req, res) => { 
+  const { file } = req.query
+  const texto = fs.readFileSync(file)
+  res.send(texto.toString()) /
 })
 
-app.use("/update", (req, res) => {
-    const { file, text } = req.query
-    // fs.appendFileSync
+app.use("/update", (req, res) => { 
+  const { file, texto } = req.query
+  fs.appendFileSync(file, texto)
+  res.send("Atualizado!!")
 })
 
-app.use("/delete", (req, res) => {
-    const { file } = req.query
-    // fs.rmSync
+app.use("/delete", (req, res) => { 
+  const { file } = req.query
+  fs.rmSync(file)
+  res.send("Deletado!!")
 })
 
-app.listen(3000,() => console.log("servidor rodando!"))
+app.listen(8080, () => console.log("Servidor funcionando!"))
