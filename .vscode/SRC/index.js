8f85e2d9bfa8b26c695027ef3879cb4e43df4400
lua <104568826+moondecria@users.noreply.github.com>
2023-06-20 14:58:32 +0000

    luana,11:59,20/06/2023

--- .vscode/SRC/index.js
+++ .vscode/SRC/index.js
@@ -4,37 +4,42 @@ const { getDatabaseInstance } = require("./database")
 
 const app = express()
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/../public'))
 
-app.use("/create", async (req, res) => { 
+app.use("/create", async (req, res) => {
   const { title, source, description, thumb } = req.query
-  const bd = await getDatabaseInstance() 
-  const resut = await bd.run(`
-    INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`,
-    [title, source, description, thumb]
-  )
-  res.send(resut)
+  const db = await getDatabaseInstance()
+  const result = await db.run(`INSERT INTO movies(title, source, description, thumb) VALUES(?, ?, ?, ?)`, [title, source, description, thumb])
+  res.json(result)
 })
 
-app.use("/read", async (req, res) => { 
+app.use("/read", async (req, res) => {
   const { id } = req.query
-  const bd = await getDatabaseInstance()
-  const result = await bd.get(`
-    SELECT * FROM movies WHERE id=?`,
-    [id])
-  res.send(result)
+  const db = await getDatabaseInstance()
+  if (id) {
+    const result = await db.get(`SELECT * FROM movies WHERE id=?`, id)
+    res.json(result)
+    return
+  }
+  const result = await db.all(`SELECT * FROM movies`)
+  res.json(result)
 })
 
-app.use("/update", (req, res) => { 
-  const { file, texto } = req.query
-  fs.appendFileSync(file, texto)
-  res.send("Atualizado!!")
+app.put("/movies", async (req, res) => {
+  const { title, source, description, thumb, id } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(
+    `UPDATE movies SET title=?, source=?, description=?, thumb=? WHERE id=?`,
+    title, source, description, thumb, id
+  )
+  res.json(result)
 })
 
-app.use("/delete", (req, res) => { 
-  const { file } = req.query
-  fs.rmSync(file)
-  res.send("Deletado!!")
+app.use("/delete", async (req, res) => {
+  const { id } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(`DELETE FROM movies WHERE id=?`, id)
+  res.json(result)
 })
 
 app.listen(3000, () => console.log("Servidor rodando!"))
